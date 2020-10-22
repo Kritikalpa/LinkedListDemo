6efb05acd4a6840913079b21c0b342791cf84707
Kriti Kalpa <krtklpsh@gmail.com>
2020-10-22 10:42:27 +0530

    [Kriti] Add. Simple add element to linked list

--- DataStructuresDemo/Program.cs
+++ DataStructuresDemo/Program.cs
@@ -7,7 +7,11 @@ namespace DataStructuresDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List demo!!!");
-            
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
